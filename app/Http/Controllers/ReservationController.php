524313d233221672f06d6d5275d282035cf9a5b4
Ruben Bratsbergm <rubenbratsberg@gmail.com>
2019-01-29 21:52:11 +0100

    Create reservation view can populate values with existing request. Added some todo comments for the future? Search fully implemented. CreateReservation soft crashes if not enough rooms.

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -13,35 +13,77 @@ use Illuminate\Validation\Rule;
  * Kommentarer til meg selv
  *      Passer $request rundt mindre.
  *      Lag servicer som gjør mindre biter av arbeidet utenfor controller
+ *      Gjør om alle whereIn om til JOINS for performance
  */
 
 class ReservationController extends Controller
 {
-    public function create()
+    public function create(Request $request)
     {
         $hotels = App\Hotel::select('name', 'hotelId')
             ->distinct('name')
             ->get();
 
-        return view('reservation.create', ['hotels' => $hotels]);
+        return view('reservation.create', [
+            'hotels' => $hotels,
+            'numRooms' => $request->numRooms,
+            'toDate' => $request->toDate,
+            'fromDate' => $request->fromDate,
+            'hotelId' => $request->hotelId
+        ]);
     }
 
     public function search()
     {
-        return view('reservation.search');
+        $cities = App\Hotel::select('city')
+            ->distinct('city')
+            ->get();
+
+        return view('reservation.search', ['cities' => $cities]);
+    }
+
+    public function performSearch(Request $request)
+    {
+        $validated = $request->validate([
+            'city' => [
+                'required',
+                'alpha_num',
+                Rule::notIn(['default']),
+            ],
+            'numRooms' => 'required|digits_between:1,100|integer',
+            'fromDate' => 'required|date|after:today',
+            'toDate' => 'required|date|after:fromDate',
+        ]);
+
+        $checkPrice = $request->maxPrice != null ? $request->maxPrice : PHP_INT_MAX;
+        $hotels = DB::table('hotels')
+            ->where('city', '=', $request->city)
+            ->where('price', '<=', $checkPrice)
+            ->orderBy('price', 'asc')
+            ->get();
+
+        // TODO: check that hotels have free rooms here and not later
+        // TODO: group free rooms by hotel and attach them to the model
+        // $availableRoomIds = array();
+        // foreach ($hotels as $hotel) {
+        //     $tmp = App\Helpers\ReservationHelpers::availableRoomsInRange($request->toDate, $request->fromDate, $hotel->hotelId);
+        //     $availableRoomIds = array_merge($availableRoomIds, $tmp);
+        // }
+        // $availableRooms = DB::table('rooms')
+        //     ->whereIn('roomId', $availableRoomIds)
+        //     ->get();
+
+        return view('reservation.searchResult', [
+            'hotels' => $hotels,
+            'fromDate' => $request->fromDate,
+            'toDate' => $request->toDate,
+            'numRooms' => $request->numRooms]
+        );
+
     }
 
     public function createReservation(Request $request)
     {
-        // all reservations with this hotel's rooms in the given time frame
-        $available = App\Helpers\ReservationHelpers::availableRoomsInRange($request->toDate, $request->fromDate, $request->hotel);
-        $availableFiltered = DB::table('rooms')
-            ->whereIn('roomId', $available)
-            ->get();
-
-        // TODO: Match numRooms mot enoughRooms. lag en custom rule og send tilbake til view.
-        $enoughRooms = $availableFiltered >= $request->numRooms;
-
         $validated = $request->validate([
             'firstName' => 'required|string|max:255',
             'lastName' => 'required|string|max:255',
@@ -56,6 +98,18 @@ class ReservationController extends Controller
             'fromDate' => 'required|date|after:today',
             'toDate' => 'required|date|after:fromDate',
         ]);
+
+        // all reservations with this hotel's rooms in the given time frame
+        $available = App\Helpers\ReservationHelpers::availableRoomsInRange($request->toDate, $request->fromDate, $request->hotel);
+        $availableFiltered = DB::table('rooms')
+            ->whereIn('roomId', $available)
+            ->get();
+
+        // TODO: Match numRooms mot enoughRooms. lag en custom rule og send tilbake til view.
+        $enoughRooms = count($availableFiltered) >= $request->numRooms;
+        if (!$enoughRooms) {
+            abort(500);
+        }
 
         $person = App\Helpers\ReservationHelpers::createOrGetPerson($request);
         $reservations = App\Helpers\ReservationHelpers::insertReservation($request, $person->personId, $request->numRooms);
